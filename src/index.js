b27b8e51c00318fb2f8175eedc3eddbba6a5756d
Juliano Ventola <ventolajuliano@gmail.com>
2021-10-21 12:45:20 -0300

    feat: filter statements by date

--- src/index.js
+++ src/index.js
@@ -78,4 +78,19 @@ app.post("/withdraw", verifyIfExistsAccountByCPF, (request, response) => {
   })
   return response.send()
 })
+
+app.get("/statement/date", verifyIfExistsAccountByCPF, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+  const dateFormat = new Date(date + " 00:00")
+
+  const statement = customer["statements"].filter(
+    (statement) =>
+      statement["created_at"].toDateString() ===
+      new Date(dateFormat).toDateString()
+  )
+
+  return response.json(statement)
+})
+
 app.listen(3333)
