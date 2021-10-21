7c152ce185e3e41b2c85c994e7a93100adc54886
Juliano Ventola <ventolajuliano@gmail.com>
2021-10-21 02:43:55 -0300

    feat: middleware to check if user exists

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json())
 
 const customers = []
 
+// Middlewares
+function verifyIfExistsAccountByCPF(request, response, next) {
+  const { cpf } = request.headers
+  const customer = customers.find((user) => user["cpf"] == cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
   const customerAlreadyExists = customers.some((user) => user["cpf"] == cpf)
@@ -24,13 +38,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send()
 })
 
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers
-  const customer = customers.find((user) => user["cpf"] == cpf)
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" })
-  }
-
+app.get("/statement/", verifyIfExistsAccountByCPF, (request, response) => {
+  const { customer } = request
   return response.json({ statements: customer["statements"] })
 })
 
